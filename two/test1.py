1c9e1013b494ae53cf0e60a5ac65cd1cf69b50f9
freelaeder <yanglo67@qq.com>
2022-04-13 09:13:47 +0800

    第二天

--- two/test1.py
+++ two/test1.py
@@ -3,9 +3,8 @@
 # date:  2022/4/13 9:09
 
 
-url = ''
-
-
+# 豆瓣电影
+url = 'https://movie.douban.com/'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
