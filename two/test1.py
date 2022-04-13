9dc1ae877902e1894595e022356d9bacac6299cc
freelaeder <yanglo67@qq.com>
2022-04-13 09:10:57 +0800

    第二天

---
+++ two/test1.py
@@ -0,0 +1,13 @@
+# author:  freelaeder
+# ----
+# date:  2022/4/13 9:09
+
+
+url = ''
+
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
+    'Accept': 'application/json'
+}
